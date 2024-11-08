0a1a69453f51d5844557f9d421cff86bc9fee8ea
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-11-07 20:05:09 -0700

    create change handler function, import axios

--- frontend/components/AuthForm.js
+++ frontend/components/AuthForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 export default function AuthForm() {
   const [isLogin, setIsLogin] = useState(true)
@@ -19,6 +20,22 @@ export default function AuthForm() {
     setPassword(event.target.value)
   }
 
+  const handlSubmit = async (event) => {
+    event.preventDefault()
+    setError('')
+    setMessage('')
+    try {
+      const { data } = await axios.post(
+        `/api/auth/${isLogin ? 'login' : 'register'}`,
+        { username , password }
+      )
+      if (isLogin) {
+        localStorage.setItem('token', data.token)
+      }
+    } catch (err) {
+
+    }
+  }
   return (
     <div className="container">
       <div aria-live="polite">{message}</div>
@@ -28,7 +45,7 @@ export default function AuthForm() {
           Switch to {isLogin ? 'Register' : 'Login'}
         </button>
       </h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username:</label>
           <input
